ce2358a64647a0b3819e76e52d48f4bd2cfa76fc
Angel Ariel Gómez Tumbaco <116834047+Kenkyo1@users.noreply.github.com>
2025-01-15 14:21:47 -0500

    guia 24

--- main/views.py
+++ main/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# Importe el decorador login_required
+from django.contrib.auth.decorators import login_required, permission_required
+
 # Importe requests y json
 import requests
 import json
 
+# Restricción de acceso con @login_required y permisos con @permission_required
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
 def index(request):
 
     # Arme el endpoint del REST API
